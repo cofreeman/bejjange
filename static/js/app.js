afc3bed6f5c275692567deaeb29b2928e1fc00cd
choijungwoo493 <70828640+choijungwoo493@users.noreply.github.com>
2021-08-19 16:34:15 +0900

    뒤로가기 버튼 구현,init할때  more버튼 버그 수정

--- static/js/app.js
+++ static/js/app.js
@@ -143,6 +143,43 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     })
+    $('#delete').click(function(){
+        var word = $('#input_text').val();
+        if (word.slice(-1) == ' '){
+            word = word.split(' ').slice(0,-2).join(' ');
+        }else {
+            word = word.split(' ').slice(0,-1).join(' ');
+        }
+        console.log($('#input_text'))
+        console.log(word)
+        $('#input_text').val(word);
+        $.ajax({
+            url: '/get_end_predictions.html',
+            type: "post",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify({
+                "input_text": $('#input_text').val(),
+                "top_k": '50',
+            })
+        }).done(function (jsondata, textStatus, jqXHR) {
+            console.log(jsondata)
+            $('#predict1').val(jsondata['bert'].split(' ')[0])
+            $('#predict2').val(jsondata['bert'].split(' ')[1])
+            $('#predict3').val(jsondata['bert'].split(' ')[2])
+            $('#predict4').val(jsondata['bert'].split(' ')[3])
+            $('#predict5').val(jsondata['bert'].split(' ')[4])
+            $('#predict6').val(jsondata['bert'].split(' ')[5])
+            $('#predict7').val(jsondata['bert'].split(' ')[6])
+            $('#predict8').val(jsondata['bert'].split(' ')[7])
+            $('#predict9').val(jsondata['bert'].split(' ')[8])
+            $('#predict10').val(jsondata['bert'].split(' ')[9])
+            $('#predict11').val(jsondata['bert'].split(' ')[10])
+            $('#predict12').val(jsondata['bert'].split(' ')[11])
+        }).fail(function (jsondata, textStatus, jqXHR) {
+            console.log(jsondata)
+        });
+    });
     $('#img-btn1').click(function(){
         var predict1 = $('#predict1').val();
         $(this).clone().appendTo('.clone-img');
@@ -485,37 +522,6 @@ jQuery(document).ready(function () {
         });
     });
     $('#img-btn12').click(function(){
-        var predict1 = $('#predict12').val();
-        $(this).clone().appendTo('.clone-img');
-        $('#input_text').append(predict1+ ' ');
-        $.ajax({
-            url: '/get_end_predictions.html',
-            type: "post",
-            contentType: "application/json",
-            dataType: "json",
-            data: JSON.stringify({
-                "input_text": $('#input_text').val(),
-                "top_k": '50',
-            })
-        }).done(function (jsondata, textStatus, jqXHR) {
-            console.log(jsondata)
-            $('#predict1').val(jsondata['bert'].split(' ')[0])
-            $('#predict2').val(jsondata['bert'].split(' ')[1])
-            $('#predict3').val(jsondata['bert'].split(' ')[2])
-            $('#predict4').val(jsondata['bert'].split(' ')[3])
-            $('#predict5').val(jsondata['bert'].split(' ')[4])
-            $('#predict6').val(jsondata['bert'].split(' ')[5])
-            $('#predict7').val(jsondata['bert'].split(' ')[6])
-            $('#predict8').val(jsondata['bert'].split(' ')[7])
-            $('#predict9').val(jsondata['bert'].split(' ')[8])
-            $('#predict10').val(jsondata['bert'].split(' ')[9])
-            $('#predict11').val(jsondata['bert'].split(' ')[10])
-            $('#predict12').val(jsondata['bert'].split(' ')[11])
-        }).fail(function (jsondata, textStatus, jqXHR) {
-            console.log(jsondata)
-        });
-    });
-    $('#img-btn13').click(function(){
         var predict1 = $('#predict13').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -546,7 +552,7 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn14').click(function(){
+    $('#img-btn13').click(function(){
         var predict1 = $('#predict14').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -577,7 +583,7 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn15').click(function(){
+    $('#img-btn14').click(function(){
         var predict1 = $('#predict15').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -608,7 +614,7 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn16').click(function(){
+    $('#img-btn15').click(function(){
         var predict1 = $('#predict16').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -639,7 +645,7 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn17').click(function(){
+    $('#img-btn16').click(function(){
         var predict1 = $('#predict17').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -670,7 +676,7 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn18').click(function(){
+    $('#img-btn17').click(function(){
         var predict1 = $('#predict18').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -701,7 +707,7 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn19').click(function(){
+    $('#img-btn18').click(function(){
         var predict1 = $('#predict19').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -732,7 +738,7 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn20').click(function(){
+    $('#img-btn19').click(function(){
         var predict1 = $('#predict20').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -763,7 +769,7 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn21').click(function(){
+    $('#img-btn20').click(function(){
         var predict1 = $('#predict21').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -794,7 +800,7 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn22').click(function(){
+    $('#img-btn21').click(function(){
         var predict1 = $('#predict22').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
@@ -825,8 +831,39 @@ jQuery(document).ready(function () {
             console.log(jsondata)
         });
     });
-    $('#img-btn23').click(function(){
+    $('#img-btn22').click(function(){
         var predict1 = $('#predict23').val();
+        $(this).clone().appendTo('.clone-img');
+        $('#input_text').append(predict1+ ' ');
+        $.ajax({
+            url: '/get_end_predictions.html',
+            type: "post",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify({
+                "input_text": $('#input_text').val(),
+                "top_k": '50',
+            })
+        }).done(function (jsondata, textStatus, jqXHR) {
+            console.log(jsondata)
+            $('#predict1').val(jsondata['bert'].split(' ')[0])
+            $('#predict2').val(jsondata['bert'].split(' ')[1])
+            $('#predict3').val(jsondata['bert'].split(' ')[2])
+            $('#predict4').val(jsondata['bert'].split(' ')[3])
+            $('#predict5').val(jsondata['bert'].split(' ')[4])
+            $('#predict6').val(jsondata['bert'].split(' ')[5])
+            $('#predict7').val(jsondata['bert'].split(' ')[6])
+            $('#predict8').val(jsondata['bert'].split(' ')[7])
+            $('#predict9').val(jsondata['bert'].split(' ')[8])
+            $('#predict10').val(jsondata['bert'].split(' ')[9])
+            $('#predict11').val(jsondata['bert'].split(' ')[10])
+            $('#predict12').val(jsondata['bert'].split(' ')[11])
+        }).fail(function (jsondata, textStatus, jqXHR) {
+            console.log(jsondata)
+        });
+    });
+    $('#img-btn23').click(function(){
+        var predict1 = $('#predict24').val();
         $(this).clone().appendTo('.clone-img');
         $('#input_text').append(predict1+ ' ');
         $.ajax({
@@ -1338,8 +1375,11 @@ jQuery(document).ready(function () {
         $('.dialog').hide();
         $('body').removeClass('active');
     });
-    $('#img-more').click(function(){
-
+    $('#delete').click(function(){
+        var word = $('#input_text').val();
+        console.log($('#input_text'))
+        console.log(word)
+        $('#input_text').val(word);
         $.ajax({
             url: '/get_end_predictions.html',
             type: "post",
@@ -1351,29 +1391,95 @@ jQuery(document).ready(function () {
             })
         }).done(function (jsondata, textStatus, jqXHR) {
             console.log(jsondata)
-            $('#predict13').val(jsondata['bert'].split(' ')[12])
-            $('#predict14').val(jsondata['bert'].split(' ')[13])
-            $('#predict15').val(jsondata['bert'].split(' ')[14])
-            $('#predict16').val(jsondata['bert'].split(' ')[15])
-            $('#predict17').val(jsondata['bert'].split(' ')[16])
-            $('#predict18').val(jsondata['bert'].split(' ')[17])
-            $('#predict19').val(jsondata['bert'].split(' ')[18])
-            $('#predict20').val(jsondata['bert'].split(' ')[19])
-            $('#predict21').val(jsondata['bert'].split(' ')[20])
-            $('#predict22').val(jsondata['bert'].split(' ')[21])
-            $('#predict23').val(jsondata['bert'].split(' ')[22])
-            $('#predict24').val(jsondata['bert'].split(' ')[23])
-            $('#predict25').val(jsondata['bert'].split(' ')[24])
-            $('#predict26').val(jsondata['bert'].split(' ')[25])
-            $('#predict27').val(jsondata['bert'].split(' ')[26])
-            $('#predict28').val(jsondata['bert'].split(' ')[27])
-            $('#predict29').val(jsondata['bert'].split(' ')[28])
-            $('#predict30').val(jsondata['bert'].split(' ')[29])
-            $('.dialog').show();
-            $('body').addClass('active');
+            $('#predict1').val(jsondata['bert'].split(' ')[0])
+            $('#predict2').val(jsondata['bert'].split(' ')[1])
+            $('#predict3').val(jsondata['bert'].split(' ')[2])
+            $('#predict4').val(jsondata['bert'].split(' ')[3])
+            $('#predict5').val(jsondata['bert'].split(' ')[4])
+            $('#predict6').val(jsondata['bert'].split(' ')[5])
+            $('#predict7').val(jsondata['bert'].split(' ')[6])
+            $('#predict8').val(jsondata['bert'].split(' ')[7])
+            $('#predict9').val(jsondata['bert'].split(' ')[8])
+            $('#predict10').val(jsondata['bert'].split(' ')[9])
+            $('#predict11').val(jsondata['bert'].split(' ')[10])
+            $('#predict12').val(jsondata['bert'].split(' ')[11])
         }).fail(function (jsondata, textStatus, jqXHR) {
             console.log(jsondata)
         });
+    });
+    $('#img-more').click(function(){
+        var word = $('#input_text').val();
+        if (word.slice(0) == ''){
+            $.ajax({
+                url: '/get_end_predictions_init.html',
+                type: "post",
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify({
+                    "input_text": $('#input_text').val(),
+                    "top_k": '50',
+                })
+            }).done(function (jsondata, textStatus, jqXHR) {
+                console.log(jsondata)
+                $('#predict13').val(jsondata['bert'].split(' ')[12])
+                $('#predict14').val(jsondata['bert'].split(' ')[13])
+                $('#predict15').val(jsondata['bert'].split(' ')[14])
+                $('#predict16').val(jsondata['bert'].split(' ')[15])
+                $('#predict17').val(jsondata['bert'].split(' ')[16])
+                $('#predict18').val(jsondata['bert'].split(' ')[17])
+                $('#predict19').val(jsondata['bert'].split(' ')[18])
+                $('#predict20').val(jsondata['bert'].split(' ')[19])
+                $('#predict21').val(jsondata['bert'].split(' ')[20])
+                $('#predict22').val(jsondata['bert'].split(' ')[21])
+                $('#predict23').val(jsondata['bert'].split(' ')[22])
+                $('#predict24').val(jsondata['bert'].split(' ')[23])
+                $('#predict25').val(jsondata['bert'].split(' ')[24])
+                $('#predict26').val(jsondata['bert'].split(' ')[25])
+                $('#predict27').val(jsondata['bert'].split(' ')[26])
+                $('#predict28').val(jsondata['bert'].split(' ')[27])
+                $('#predict29').val(jsondata['bert'].split(' ')[28])
+                $('#predict30').val(jsondata['bert'].split(' ')[29])
+                $('.dialog').show();
+                $('body').addClass('active');
+            }).fail(function (jsondata, textStatus, jqXHR) {
+                console.log(jsondata)
+            });
+        }else{
+            $.ajax({
+                url: '/get_end_predictions.html',
+                type: "post",
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify({
+                    "input_text": $('#input_text').val(),
+                    "top_k": '50',
+                })
+            }).done(function (jsondata, textStatus, jqXHR) {
+                console.log(jsondata)
+                $('#predict13').val(jsondata['bert'].split(' ')[12])
+                $('#predict14').val(jsondata['bert'].split(' ')[13])
+                $('#predict15').val(jsondata['bert'].split(' ')[14])
+                $('#predict16').val(jsondata['bert'].split(' ')[15])
+                $('#predict17').val(jsondata['bert'].split(' ')[16])
+                $('#predict18').val(jsondata['bert'].split(' ')[17])
+                $('#predict19').val(jsondata['bert'].split(' ')[18])
+                $('#predict20').val(jsondata['bert'].split(' ')[19])
+                $('#predict21').val(jsondata['bert'].split(' ')[20])
+                $('#predict22').val(jsondata['bert'].split(' ')[21])
+                $('#predict23').val(jsondata['bert'].split(' ')[22])
+                $('#predict24').val(jsondata['bert'].split(' ')[23])
+                $('#predict25').val(jsondata['bert'].split(' ')[24])
+                $('#predict26').val(jsondata['bert'].split(' ')[25])
+                $('#predict27').val(jsondata['bert'].split(' ')[26])
+                $('#predict28').val(jsondata['bert'].split(' ')[27])
+                $('#predict29').val(jsondata['bert'].split(' ')[28])
+                $('#predict30').val(jsondata['bert'].split(' ')[29])
+                $('.dialog').show();
+                $('body').addClass('active');
+            }).fail(function (jsondata, textStatus, jqXHR) {
+                console.log(jsondata)
+            });
+        }
     });
 
 })
